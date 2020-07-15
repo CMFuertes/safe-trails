5963575c4befafbb6231e4dd69455628458cd665
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-07-15 16:29:59 -0400

    fixing js errors

--- js/init.js
+++ js/init.js
@@ -8,4 +8,27 @@
   $(document).ready(function(){
     $('select').formSelect();
   });
+
+  $("#submit").on("click", function(event) {
+
+    event.preventDefault();       
+    console.log("inside click")
+
+    var selectedState = parseInt($("#myselection").val() );     
+    console.log(selectedState);
+
+      var queryURL = "https://covidtracking.com/api/states";
+      console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) { //async code block, it's going to run when you get the response back 
+    console.log(response);
+    console.log("The State/Territory of:", response[selectedState].state);
+    console.log("Had an increase of", response[selectedState].positiveIncrease, "cases");
+    console.log("Total positive cases =", response[selectedState].positive);
+    console.log("Data Updated on =", response[selectedState].dateChecked);
+  });
+        });
 })(jQuery); // end of jQuery name space

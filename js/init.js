d8197c6a3a41c556a9a93b24a8374d792924bb41
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-07-15 20:40:44 -0400

    js changes

--- js/init.js
+++ js/init.js
@@ -30,21 +30,18 @@
     console.log("Data Updated on =", response[selectedState].dateChecked);
     $("#following-data").text("Based on your selection, the following state's information is as follows:");
     $("#state-name").html("The State/Territory of: " + (response[selectedState].state));
-    $("#state-increase").html("Had an increase of: " +(response[selectedState].positiveIncrease) + " cases since, " + (response[selectedState].dateChecked));
+    $("#state-increase").html("Had an increase of: " +(response[selectedState].positiveIncrease) + " cases since,  " + (response[selectedState].dateChecked));
     $("#total-cases").html("Total positive cases: " + (response[selectedState].positive));
 
-    
+ 
     
   });
         });
 
         $(".saveState").on("click", function () {
           console.log("save me");
-          var savedState = $(this).siblings("#state-name").val();
-          console.log(savedState);
-          var stateInfo = $(this).parent().attr(".saved-state1");
-          console.log(stateInfo);
-          localStorage.setItem(stateInfo), JSON.stringify(savedState);
+             var stateAbr = response[selectedState].state;
+             localStorage.setItem("stateAbr", JSON.stringify(savedState));
 
         });
 

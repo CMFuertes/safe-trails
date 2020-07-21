dc15727749c9c11f1175320767bba87a939e4f9b
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-07-21 01:06:19 -0400

    added a modal to request increase of search radius if no results found

--- js/script1.js
+++ js/script1.js
@@ -15,29 +15,25 @@ $(document).ready(function () {
     var APIkey1 = "f2e73a675d880326530db1f8aee7437b";
     var APIkey2 = "f2e73a675d880326530db1f8aee7437b";
     var APIkey3;
-    var radius=10;
+    var radius = 10;
     var cityDebug;
-    var perPage=10;
-    
-    
+    var perPage = 10;
+
+
     // on click function for updating  the below 3 variables from user preferences modal
     $(".modal-close").on("click", function (APIkey3) {
         // APIkey3 = $("#APIkey3").val().trim();
         radius = $("#search-radius").val().trim();
         perPage = $("#search-results").val().trim();
-        // console.log(APIkey3);
-        // console.log(radius);
-        // console.log(perPage);
+
         $(".pererences").html(`radius = ${radius} miles, results = ${perPage} trails.`);
     });
     $(".pererences").html(`radius = ${radius} miles, results = ${perPage} trails.`);
 
     // module for connecting the debugMode boolean to the debugMode toggle
     debugModePicker = $("#debugModePicker");
-
     debugModePicker.click(function () {
         debugMode = !debugMode;
-        // console.log("debugMode : " + debugMode);
     });
 
 
@@ -48,28 +44,19 @@ $(document).ready(function () {
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey1;
         cityDebug = localStorage.getItem("city");
 
-        // console.log(cityDebug);
-        // console.log(city);
-
         // Creating an AJAX call for the specific city button being clicked
         //if the program is in a debugMode 
         // the call would be avoided and data red from local storage instead
         if (debugMode === false) {
-            // console.log("ajax call 1");
-
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
                 // preparing the div that will hold the city and date displayed
-                // console.log("response 1 :");
-                // console.log(response);
                 localStorage.setItem("responseString", JSON.stringify(response));
-                // console.log("ajax call ");
                 // getting the lon and lat for the given city to be used for the next 2 api calls
                 lat = response.city.coord.lat;
                 lon = response.city.coord.lon;
-
                 $(".fd-forecast").removeClass("no-show");
                 findBikeTrails(lon, lat);
                 displayUv();
@@ -78,12 +65,8 @@ $(document).ready(function () {
         else {
             // debug Mode  uses saved last responce for debugging , no actual ajax calls=============
             debugResponse = JSON.parse(localStorage.getItem("responseString"));
-            // console.log(debugResponse);
-            // console.log("debugResponse");
             lat = debugResponse.city.coord.lat;
             lon = debugResponse.city.coord.lon;
-            // console.log("lat  " + lat);
-            // console.log("lon  " + lon);
             $(".fd-forecast").removeClass("no-show");
             findBikeTrails();
             displayUv();
@@ -108,9 +91,6 @@ $(document).ready(function () {
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-                // console.log("response 2 :");
-                // console.log("ajax call 2 ");
-                // console.log(response);
                 localStorage.setItem("responseString1", JSON.stringify(response));
                 createForecast(response);
             });
@@ -118,8 +98,6 @@ $(document).ready(function () {
         }
         else {
             debugResponse1 = JSON.parse(localStorage.getItem("responseString1"));
-            // console.log(debugResponse1);
-            // console.log("debugResponse1");
             response = debugResponse1
             createForecast(response);
 
@@ -198,19 +176,13 @@ $(document).ready(function () {
                 "x-rapidapi-key": APIkey3
             }
         }
-        // console.log(settings);
-        // console.log(perPage);
-        // console.log(radius);
-
         //if the program is in a debugMode or if the city already is in local storage , 
         // the call would be avoided and data red from local storage instead
         if (debugMode === false && city !== cityDebug) {
             cityDebug = localStorage.setItem("city", city);
             $.ajax(settings).done(function (response) {
                 localStorage.setItem("responseString3", JSON.stringify(response));
-                // console.log("response 3 :");
-                // console.log(response);
-                // console.log("ajax call 3");
+               console.log(response);
                 fillIntrailCards(response);
             });
 
@@ -218,8 +190,6 @@ $(document).ready(function () {
         else {
             // same as above but uses a saved object from local storage to avoid additional calls during debugs and testing
             debugResponse3 = JSON.parse(localStorage.getItem("responseString3"));
-            // console.log(debugResponse3);
-            // console.log("debugResponse3");
             response = debugResponse3;
             fillIntrailCards(response);
         }
@@ -230,83 +200,98 @@ $(document).ready(function () {
         $("#bike-trails-list").empty();
 
         // forloop for creating and filling in of the card elements
-        for (var i = 0; i < response.data.length; i++) {
+        if (response.data === null) {
+             $('.modal').modal();
+             $('#modal2').modal('open'); 
+             $(".modal2-close").on("click", function (open) {
+                $('#modal1').modal('open'); 
+            });
+
+        localStorage.setItem("city", "badRadius");
+
+        }
+        else {
+           
+        
+            for (var i = 0; i < response.data.length; i++) {
 
 
-            // creating variables and assigning values from the trail api object(fresh or from localstorage)
-            var aCity = response.data[i].city
-            var aState = response.data[i].region;
-            var aName = response.data[i].name;
-            var aDirections = response.data[i].directions;
-            var aDescription = response.data[i].description;
-            var aLength = response.data[i].length;
-            var aRaiting = response.data[i].rating;
-            var aImg = response.data[i].thumbnail;
-            var aUrl = response.data[i].url;
-            var aId = response.data[i].id;
-            var aDifficulty = response.data[i].difficulty;
-            // creating the new elements for the dom
-            var news12m7 = $('<div class="col s7 m6">');
-            var newCard = $('<div class="card">');
-            var newCardImg = $('<div class="card-image">');
-            var newCardCont = $('<div class="card-content">');
-            var pOne = $('<p>');
-            var s1 = $('<span class="card-title">');
-            var s2 = $('<span class="card-title">');
-            var pThree = $('<p>');
-            var pFour = $('<p>');
-            var pFive = $('<p>');
-            var pSix = $('<p>');
-            var pSeven = $('<p>');
-            var pEight = $('<p>');
-            var pNine = $('<p>');
-            var iOne = $('<img class="activator">');
-            var l1 = $('<a>');
-            var newCardReveal = $('<div>');
+                // creating variables and assigning values from the trail api object(fresh or from localstorage)
+                var aCity = response.data[i].city
+                var aState = response.data[i].region;
+                var aName = response.data[i].name;
+                var aDirections = response.data[i].directions;
+                var aDescription = response.data[i].description;
+                var aLength = response.data[i].length;
+                var aRaiting = response.data[i].rating;
+                var aImg = response.data[i].thumbnail;
+                var aUrl = response.data[i].url;
+                var aId = response.data[i].id;
+                var aDifficulty = response.data[i].difficulty;
+                // creating the new elements for the dom
+                var news12m7 = $('<div class="col s7 m6">');
+                var newCard = $('<div class="card">');
+                var newCardImg = $('<div class="card-image">');
+                var newCardCont = $('<div class="card-content">');
+                var pOne = $('<p>');
+                var s1 = $('<span class="card-title">');
+                var s2 = $('<span class="card-title">');
+                var pThree = $('<p>');
+                var pFour = $('<p>');
+                var pFive = $('<p>');
+                var pSix = $('<p>');
+                var pSeven = $('<p>');
+                var pEight = $('<p>');
+                var pNine = $('<p>');
+                var iOne = $('<img class="activator">');
+                var l1 = $('<a>');
+                var newCardReveal = $('<div>');
 
-            // assigning values , text , html, attibutes and classes accordingly
-            $(newCardImg).addClass("waves-effect waves-block waves-light");
-            $(s1).addClass("activator grey-text text-darken-4");
-            $(s2).addClass("grey-text text-darken-4");
-            $(s1).html(`Name: ${aName}<i class="material-icons right">Click for More</i>`);
-            $(s2).html(`Name: ${aName}<i class="material-icons right">close</i>`);
-            $(newCardReveal).addClass("card-reveal");
-            $(pOne).text("Location of the trail: " + aCity + "," + aState);
-            pThree.html("Directions:  " + aDirections);
-            pFour.html("Description:  " + aDescription);
-            pFive.text("Length of the trail:  " + aLength);
-            pSix.text("Rating:  " + aRaiting);
-            iOne.attr("src", aImg);
-            iOne.attr("width", "330");
-            iOne.attr("height", "257");
-            iOne.attr("alt", "thumbnail");
-            pSeven.text("id: " + aId);
-            pEight.text("difficulty: " + aDifficulty);
-            l1.attr("href", aUrl);
-            l1.attr("target", "_blank");
-            l1.text("Link to the trail website");
-            pNine.html(l1);
+                // assigning values , text , html, attibutes and classes accordingly
+                $(newCardImg).addClass("waves-effect waves-block waves-light");
+                $(s1).addClass("activator grey-text text-darken-4");
+                $(s2).addClass("grey-text text-darken-4");
+                $(s1).html(`Name: ${aName}<i class="material-icons right">Click for More</i>`);
+                $(s2).html(`Name: ${aName}<i class="material-icons right">close</i>`);
+                $(newCardReveal).addClass("card-reveal");
+                $(pOne).text("Location of the trail: " + aCity + "," + aState);
+                pThree.html("Directions:  " + aDirections);
+                pFour.html("Description:  " + aDescription);
+                pFive.text("Length of the trail:  " + aLength);
+                pSix.text("Rating:  " + aRaiting);
+                iOne.attr("src", aImg);
+                iOne.attr("width", "330");
+                iOne.attr("height", "257");
+                iOne.attr("alt", "thumbnail");
+                pSeven.text("id: " + aId);
+                pEight.text("difficulty: " + aDifficulty);
+                l1.attr("href", aUrl);
+                l1.attr("target", "_blank");
+                l1.text("Link to the trail website");
+                pNine.html(l1);
 
-            //appending all  completed new elements on the page
-            $(newCardReveal).append(s2);
-            $(newCardReveal).append(pFour);
-            $(newCardReveal).append(pFive);
-            $(newCardReveal).append(pSix);
-            $(newCardReveal).append(pEight);
-            $(newCardReveal).append(pThree);
-            $(newCardReveal).append(pOne);
-            $(newCardCont).append(s1);
-            $(newCardCont).append(pNine);
-            $(newCard).append(pSeven);
-            $(newCard).append(newCardReveal);
-            $(newCardImg).append(iOne);
-            $(newCard).append(newCardImg);
-            $(newCard).append(newCardCont);
-            $(news12m7).append(newCard);
-            $("#bike-trails-list").append(news12m7);
-
+                //appending all  completed new elements on the page
+                $(newCardReveal).append(s2);
+                $(newCardReveal).append(pFour);
+                $(newCardReveal).append(pFive);
+                $(newCardReveal).append(pSix);
+                $(newCardReveal).append(pEight);
+                $(newCardReveal).append(pThree);
+                $(newCardReveal).append(pOne);
+                $(newCardCont).append(s1);
+                $(newCardCont).append(pNine);
+                $(newCard).append(pSeven);
+                $(newCard).append(newCardReveal);
+                $(newCardImg).append(iOne);
+                $(newCard).append(newCardImg);
+                $(newCard).append(newCardCont);
+                $(news12m7).append(newCard);
+                $("#bike-trails-list").append(news12m7);
+            }
         }
 
     }
+
+
 
 });
